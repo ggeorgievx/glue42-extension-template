0dadf55a65800eeaa91a74bcbe94485cd3a54a45
Georgi Georgiev <georgi.georgiev@tick42.com>
2019-06-25 12:48:40 +0300

    Include an example use of the injectedGlue variable inside of extension.js

--- extension.js
+++ extension.js
@@ -1,1 +1,13 @@
 /* global injectedGlue */
+
+// Use ```injectedGlue``` for access to all the [Glue42 APIs](https://docs.glue42.com/g4e/reference/glue/latest/glue/index.html)
+
+// Example:
+
+// injectedGlue.agm.invoke('T42.GNS.Publish.RaiseNotification', {
+//   notification: {
+//     title: 'Notification Title',
+//     severity: 'Low',
+//     description: 'Notification Description'
+//   }
+// });
